0d6eb65cc78bcae42f2ea74937c8a92b4f5bbe84
jiangzj1008 <jiangzj1008@outlook.com>
2018-03-07 18:30:28 +0800

    init

--- src/app.js
+++ src/app.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import Login from './page/login'
 
 class Main extends Component {
     render() {
         return (
-            <div>
-                <h4>微博</h4>
-            </div>
+            <Router>
+                <div>
+                    <ul>
+                        <li><Link to="/login">Login</Link></li>
+                    </ul>
+                    <Route path="/login" component={Login}/>
+                </div>
+            </Router>
         )
     }
 }
---
+++ src/page/login.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react'
+
+class Login extends Component {
+    state = {
+        username: "",
+        password: "",
+    }
+
+    handleSubmit = () => {
+        const text = this.state.text.trim()
+        if (text.length > 0) {
+            // this.props.addWeibo(text)
+        }
+        let newState = {
+            username: "",
+            password: "",
+        }
+        this.setState(newState)
+    }
+
+    handleChange = e => {
+        const val = e.target.value
+        const key = e.target.name
+        let newState = {
+            [key]: val
+        }
+        this.setState(newState)
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>登录</h4>
+                <input
+                    placeholder="用户名"
+                    name="username"
+                    onChange={this.handleChange}
+                    value={this.state.username}
+                />
+                <input
+                    placeholder="密码"
+                    name="password"
+                    onChange={this.handleChange}
+                    value={this.state.password}
+                />
+                <button onClick={this.handleSubmit}>登录</button>
+            </div>
+        )
+    }
+}
+
+export default Login;
